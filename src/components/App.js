09160faa69d9288090aa69ea96643d97b20f79e1
Md Imtiyaz Ahmed <ahmedmdimtiyaz@gmail.com>
2019-04-24 13:28:24 +0530

    First Commit - React-Redux-Application

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './home/HomePage';
+import AboutPage from './about/AboutPage';
+import Header from './common/Header';
+import PageNotFound from './PageNotFound';
+import CoursesPage from './courses/CoursesPage';
+import ManangeCourse from './courses/ManangeCourse';
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+function App(){
+    return (
+        <div className="container-fluid">
+            <Header />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/courses" component={CoursesPage} />
+                <Route path="/course/:slug" component={ManangeCourse} />
+                <Route path="/course" component={ManangeCourse} />
+                <Route component={PageNotFound} />
+            </Switch>
+            <ToastContainer autoClose={3000} hideProgressBar/>
+        </div>
+    )
+}
+
+export default App;
